33bb7d41237edec96f2e09b12de0e6bb74d41b3a
Suman Das <sum4ndas@gmail.com>
2022-10-18 23:16:04 +0530

    Implement showNextImage() function.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const imgList = ['bird1.jpg', 'bird2.jpg', 'bird3.jpg',
+                 'bird4.jpg', 'bird5.jpg'];
+
+const imgListLength = imgList.length;
+
+
+function showNextImage() {
+    
+    const imgLive = document.getElementById("carousel");
+    const imgLiveSrc = imgLive.getAttribute("src").slice(9);
+
+    for(let i = 0; i < imgListLength - 1; i++) {
+        if (imgList[i] == imgLiveSrc) {
+            imgLive.src = `./images/${imgList[i+1]}`;
+        }
+    }
+}
