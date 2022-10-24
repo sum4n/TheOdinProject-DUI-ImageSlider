b486d83820d945b7f83575b3cfd831523551e484
Suman Das <sum4ndas@gmail.com>
2022-10-24 20:16:19 +0530

    Mark/Unmark nav dots on image load and image change.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const imgListLength = imgList.length;
 
 const previousImage = document.getElementById("previousImage");
 const nextImage = document.getElementById("nextImage");
+const navDotsList = document.querySelectorAll(".dots");
 
 function getImageIndex() {
     const imgLive = document.getElementById("carousel");
@@ -24,14 +25,22 @@ function imgTransition(img) {
     }
 }
 
+function markUnmarkNavDot(index, liveImgIndex) {
+    navDotsList[index].style.background = "white";
+    navDotsList[liveImgIndex].style.background = "";
+}
+
+
 function showNextImage() {
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
     let [imgLive, liveIndex] = getImageIndex();
     
     if (liveIndex === 4) {
         imgLive.src =  `./images/${imgList[0]}`;
+        markUnmarkNavDot(0, liveIndex);
     } else {
         imgLive.src = `./images/${imgList[liveIndex + 1]}`;
+        markUnmarkNavDot(liveIndex + 1, liveIndex);
     }
 
     imgTransition(imgLive);
@@ -43,8 +52,10 @@ function showPreviousImage() {
     
     if (liveIndex === 0) {
         imgLive.src =  `./images/${imgList[4]}`;
+        markUnmarkNavDot(4, liveIndex);
     } else {
         imgLive.src = `./images/${imgList[liveIndex - 1]}`;
+        markUnmarkNavDot(liveIndex - 1, liveIndex);
     }
     
     imgTransition(imgLive);
@@ -56,4 +67,9 @@ previousImage.addEventListener("click", () => {
 
 nextImage.addEventListener("click", () => {
     showNextImage();
+})
+
+window.addEventListener("load", () => {
+    let index = getImageIndex()[1];
+    markUnmarkNavDot(index, 2);
 })
